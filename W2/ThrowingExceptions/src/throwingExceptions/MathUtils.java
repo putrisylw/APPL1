aa5e116bc5a30d82ece53692f9ffb98eac05dd19
putrisylw <putri.syalwa.tif419@polban.ac.id>
2021-03-24 16:29:12 +0700

    Add Factorials and MathUtils class

---
+++ W2/ThrowingExceptions/src/throwingExceptions/MathUtils.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package throwingExceptions;
+
+/**
+ *
+ * @author Putri Syalwa
+ * 191524054
+ */
+// ****************************************************************
+// MathUtils.java
+//
+// Provides static mathematical utility functions.
+//
+// ****************************************************************
+public class MathUtils
+{
+//-------------------------------------------------------------
+// Returns the factorial of the argument given
+//-------------------------------------------------------------
+public static int factorial(int n)
+{
+    int fac = 1;
+    for (int i=n; i>0; i--)
+        fac *= i;
+        return fac;
+    }
+}
