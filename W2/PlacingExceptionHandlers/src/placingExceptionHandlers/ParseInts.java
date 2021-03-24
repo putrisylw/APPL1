641432aa811d9c06117976a1031f9709c4117f0e
putrisylw <putri.syalwa.tif419@polban.ac.id>
2021-03-24 12:42:36 +0700

    Add ParseInts

---
+++ W2/PlacingExceptionHandlers/src/placingExceptionHandlers/ParseInts.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package placingExceptionHandlers;
+
+/**
+ *
+ * @author Putri Syalwa
+ * 191524054
+ */
+// ****************************************************************
+// ParseInts.java
+//
+// Reads a line of text and prints the integers in the line.
+//
+// ****************************************************************
+import java.util.Scanner;
+public class ParseInts{
+    public static void main(String[] args){
+        int val, sum=0;
+        Scanner scan = new Scanner(System.in);
+        String line;
+        System.out.println("Enter a line of text");
+        Scanner scanLine = new Scanner(scan.nextLine());
+        while (scanLine.hasNext()){
+            val = Integer.parseInt(scanLine.next());
+            sum += val;
+        }
+        System.out.println("The sum of the integers on this line is " + sum);
+    }
+}
