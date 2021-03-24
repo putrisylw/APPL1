721683b3aa2bf1a3227c6a9c90c35aff983bb00c
putrisylw <putri.syalwa.tif419@polban.ac.id>
2021-03-24 18:28:43 +0700

    Update Throwing Exceptions

--- W2/ThrowingExceptions/src/throwingExceptions/MathUtils.java
+++ W2/ThrowingExceptions/src/throwingExceptions/MathUtils.java
@@ -21,14 +21,16 @@ public class MathUtils{
     // Returns the factorial of the argument given
     //-------------------------------------------------------------
     public static int factorial(int n) throws IllegalArgumentException{
-        if(n >= 0){
+        if(n > 16){
+            throw new IllegalArgumentException("Argument over 16 are invalid.");
+        }else if(n < 0){
+            throw new IllegalArgumentException("A negative argument are invalid.");
+        } else{
             int fac = 1;
             for (int i=n; i>0; i--){
                 fac *= i;
             }
             return fac;
-        } else{
-            throw new IllegalArgumentException("Invalid input.");
         }
     }
 }
